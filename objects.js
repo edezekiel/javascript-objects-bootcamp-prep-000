5d5311fdced6014234883a40e97aaeaf10d1087c
ed.a.ezekiel@gmail.com <ed.a.ezekiel@gmail.com>
2018-09-07 20:12:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -15,6 +15,5 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
-  return playlist
+  delete playlist.artistName
 }
